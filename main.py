3bfc8474e6b7e369917972316d344c02349a5503
Lyubomir <lyubomirshvetsov@ynadex.com>
2023-04-16 13:02:01 +0300

    Таблица работ

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-from flask import Flask
+from flask import Flask, render_template
 from data.db_session import global_init, create_session
 from data.__all_models import *
+import json
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
@@ -26,7 +27,42 @@ def main():
     session = create_session()
     session.add(job)
     session.commit()
-    app.run()
+    app.run(host='127.0.0.1', port='5000')
+
+
+@app.route('/<string:title>')
+@app.route('/index/<string:title>')
+def index(title):
+    return render_template('index.html', title=title)
+
+
+@app.route('/training/<string:prof>')
+def courses(prof):
+    return render_template('courses.html', prof=prof, title="Подготовительные пунткты")
+
+
+@app.route('/list_prof/<string:list_type>')
+def duties(list_type):
+    with open("static/json/duties.json", encoding="utf-8") as file:
+        p = json.load(file)
+    return render_template("professions_list.html", list_type=list_type, duties=p, title="Список профессий")
+
+
+@app.route('/answer')
+@app.route('/answer_auto')
+def answer():
+    with open("static/json/anketa.json", encoding="utf-8") as file:
+        p = json.load(file)
+    with open("static/json/keys.json", encoding="utf-8") as file:
+        n = json.load(file)
+    return render_template("auto_answer.html", form=p, keys=n)
+
+
+@app.route('/')
+def works_journal():
+    all_jobs = create_session().query(Job).all()
+    return render_template('works_journal.html', jobs=all_jobs, create_session=create_session, user_class=User)
+
 
 if __name__ == '__main__':
     main()
